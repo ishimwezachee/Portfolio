ead324711f4824ee0f52029ebff92413798fb082
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-05 12:07:22 +0200

    fix linters

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const humberg = document.querySelector(".navigation");
-const navList = document.querySelector(".nav-list");
-const icon = document.querySelector(".fa-bars");
-const times = document.querySelector(".times");
-const navLink = document.querySelectorAll(".nav-link");
+const humberg = document.querySelector('.navigation');
+const navList = document.querySelector('.nav-list');
+const times = document.querySelector('.times');
+const navLink = document.querySelectorAll('.nav-link');
+
 function closeNavItems() {
-  navList.classList.replace("column", "nav-list");
-  navList.style.display = "";
+  navList.classList.replace('column', 'nav-list');
+  navList.style.display = '';
 }
-humberg.addEventListener("click", () => {
-  navList.classList.replace("nav-list", "column");
-  times.classList.add("fa-times");
-  navList.style.display = "block";
+humberg.addEventListener('click', () => {
+  navList.classList.replace('nav-list', 'column');
+  times.classList.add('fa-times');
+  navList.style.display = 'block';
 });
 
-times.addEventListener("click", () => {
+times.addEventListener('click', () => {
   closeNavItems();
 });
 
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
     if (window.innerWidth < 600) {
       closeNavItems();
     }
-  })
-);
+  });
+});
