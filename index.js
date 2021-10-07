f41145d0e309e494ffec1b9db0086491e48da171
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-07 08:57:23 +0200

    store the data in the array and render them based on the array

--- index.js
+++ index.js
@@ -5,15 +5,62 @@ const navLink = document.querySelectorAll('.nav-link');
 const modal = document.querySelector('.modal');
 const btn = document.querySelectorAll('.btn-project');
 
-const modalData = {
-  title: 'Keep track of hundreds of components',
-  description:
-    ' Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
-  image: 'images/project-image.png',
-  techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
-  liveVersion: 'https://github.com/ishimwezachee/Portfolio',
-  sourceUrl: 'https://github.com/ishimwezachee',
-};
+const data = [
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+    '  0 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+      ' 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+      '2 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+      ' 3 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+      ' 4 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+  {
+    title: 'Keep track of hundreds of components',
+    description:
+      '  5 Lorem ipsum dolor sit amet consectetur, adipisicing elit In architecto aliquam accusantium magnam, amet accusamus maxime sit voluptatum laboriosam, nulla optio, repellendus nobis a esse sunt. Commodi amet in corporis!',
+    image: 'images/project-image.png',
+    techinologies: ['Ruby on Rails', 'Css', 'Javascript'],
+    liveVersion: 'https://github.com/ishimwezachee/Portfolio',
+    sourceUrl: 'https://github.com/ishimwezachee',
+  },
+];
 
 function closeNavItems() {
   navList.classList.replace('column', 'nav-list');
@@ -37,61 +84,63 @@ navLink.forEach((link) => {
   });
 });
 
-btn.forEach((button) => {
-  button.addEventListener('click', () => {
-    modal.style.display = 'block';
-  });
-});
-
 window.addEventListener('DOMContentLoaded', () => {
-  let project = `
-  <div class='modal-content'>
-    <span class='close'>&times;</span>
-    <div class='modal-container'>
-      <div class='pop-card'>
-          <img class='card-image' src=${modalData.image} alt='img-modal' />
-        <div class='text-btns'>
-          <p class='track'>${modalData.title}</p>
-          <div class='check2'>
-          <button class='check-detail'><a href=${modalData.liveVersion} class='social-link'>
-          See Live <span><i class='far fa-arrow-alt-circle-up'></i></span>
-        </a></button>
-        <button class='check-detail'>
-          <a href=${modalData.sourceUrl} class='social-link'>
-            See source <span<i class='fab fa-github'></i></i></span>
-          </a>
-        </button>
+  btn.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      modal.style.display = 'block';
+      for (let i = 0; i < data.length; i += 1) {
+        const modalData = data[index];
+        let project = `
+    <div class='modal-content'>
+      <span class='close'>&times;</span>
+      <div class='modal-container'>
+        <div class='pop-card'>
+            <img class='card-image' src=${modalData.image} alt='img-modal' />
+          <div class='text-btns'>
+            <p class='track'>${modalData.title}</p>
+            <div class='check2'>
+            <button class='check-detail'><a href=${modalData.liveVersion} class='social-link'>
+            See Live <span><i class='far fa-arrow-alt-circle-up'></i></span>
+          </a></button>
+          <button class='check-detail'>
+            <a href=${modalData.sourceUrl} class='social-link'>
+              See source <span<i class='fab fa-github'></i></i></span>
+            </a>
+          </button>
+            </div>
           </div>
-        </div>
-        <ul class='list-btns'>
-        `;
+          <ul class='list-btns'>
+          `;
 
-  modalData.techinologies.forEach((language) => {
-    project += `<li class='list-btn'>${language}</li>`;
-  });
+        modalData.techinologies.forEach((language) => {
+          project += `<li class='list-btn'>${language}</li>`;
+        });
 
-  project += `
-          </ul>
-          <p class='text-describtion'>${modalData.description}</p>
-          <div class='check'>
-          <button class='check-detail'><a href=${modalData.liveVersion} class='social-link'>
-          See Live <span><i class='far fa-arrow-alt-circle-up'></i></span>
-        </a></button>
-        <button class='check-detail'>
-          <a href=${modalData.sourceUrl} class='social-link'>
-            See source <span<i class='fab fa-github'></i></i></span>
-          </a>
-        </button>
+        project += `
+            </ul>
+            <p class='text-describtion'>${modalData.description}</p>
+            <div class='check'>
+            <button class='check-detail'><a href=${modalData.liveVersion} class='social-link'>
+            See Live <span><i class='far fa-arrow-alt-circle-up'></i></span>
+          </a></button>
+          <button class='check-detail'>
+            <a href=${modalData.sourceUrl} class='social-link'>
+              See source <span<i class='fab fa-github'></i></i></span>
+            </a>
+          </button>
+            </div>
           </div>
-        </div>
+      </div>
     </div>
-  </div>
-    `;
-  modal.innerHTML = project;
+      `;
+        modal.innerHTML = project;
 
-  const span = document.querySelector('.close');
-  span.addEventListener('click', () => {
-    modal.style.display = 'none';
+        const span = document.querySelector('.close');
+        span.addEventListener('click', () => {
+          modal.style.display = 'none';
+        });
+      }
+    });
   });
 });
 
