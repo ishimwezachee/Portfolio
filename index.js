4e87624eec65287c7ab67f4f00deafe379cf7297
ishimwezachee <zacheeishimwe@gmail.com>
2021-10-07 12:18:36 +0200

    fix linters

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const navLink = document.querySelectorAll('.nav-link');
 const modal = document.querySelector('.modal');
 const btn = document.querySelectorAll('.btn-project');
 const contactMeForm = document.querySelector('#contact-me-form');
-const email = contactMeForm.elements['email'];
+const { email } = contactMeForm.elements;
 const Error = document.querySelector('.Error_msg');
 
 const data = [
@@ -146,14 +146,14 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  contactMeForm.addEventListener('submit', function(event){
+  contactMeForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    if(email.value === email.value.toLowerCase()){
+    if (email.value === email.value.toLowerCase()) {
       event.target.submit();
-    }else{
-    Error.style.display ="block"
+    } else {
+      Error.style.display = 'block';
     }
-    });
+  });
 });
 
 window.addEventListener('click', (event) => {
@@ -161,4 +161,3 @@ window.addEventListener('click', (event) => {
     modal.style.display = 'none';
   }
 });
-
